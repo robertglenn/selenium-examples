c4d92808c4485fa84191a0c86a8426d82fd4e7ad
Robert Glenn <robertcharlesglenn@gmail.com>
2018-02-12 15:26:16 -0800

    changing to lambda functions to remove the weird world = this madness

--- google.com/features/step_definitions/search_selenium.js
+++ google.com/features/step_definitions/search_selenium.js
@@ -8,34 +8,30 @@ const SELENIUM_TIMEOUT = 5 * 1000;
 defineSupportCode(function ({ Given, When, Then }) {
 
   Given('I am on the google home page - one', function () {
-    const world = this;
-    return world.driver.get('http://www.google.com').then(function () {
-      return world.driver.wait(until.elementLocated(By.css('body')), SELENIUM_TIMEOUT);
+    return this.driver.get('http://www.google.com').then(() => {
+      return this.driver.wait(until.elementLocated(By.css('body')), SELENIUM_TIMEOUT);
     });
   });
 
   When('I enter selenium string into the search element', function () {
-    const world = this;
-    return world.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then(function (searchElement) {
+    return this.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then((searchElement) => {
       return searchElement.sendKeys('selenium');
     });
   });
 
   When('I click Google Search button', function () {
-    const world = this;
-    world.driver.wait(until.elementLocated(By.css('#tsf > div.tsf-p')), SELENIUM_TIMEOUT).then(function (randomDivToHideSuggestions) {
-      return randomDivToHideSuggestions.click().then(function () {
-        return world.driver.wait(until.elementLocated(By.css('#tsf > div.tsf-p > div.jsb > center > input[type="submit"]:nth-child(1)')), SELENIUM_TIMEOUT).then(function (searchSubmit) {
-          return searchSubmit.click();
-        });
+    this.driver.wait(until.elementLocated(By.css('#tsf > div.tsf-p')), SELENIUM_TIMEOUT).then((randomDivToHideSuggestions) => {
+      return randomDivToHideSuggestions.click();
+    }).then(() => {
+      return this.driver.wait(until.elementLocated(By.css('#tsf > div.tsf-p > div.jsb > center > input[type="submit"]:nth-child(1)')), SELENIUM_TIMEOUT).then((searchSubmit) => {
+        return searchSubmit.click();
       });
     });
   });
 
   Then('I should see results including the seleniumhq website', function () {
-    const world = this;
-    return world.driver.wait(until.elementLocated(By.css('div:nth-child(1) > div > div > div > div > h3 > a')), SELENIUM_TIMEOUT).then(function (link) {
-      return link.getText().then(function (linkText) {
+    return this.driver.wait(until.elementLocated(By.css('div:nth-child(1) > div > div > div > div > h3 > a')), SELENIUM_TIMEOUT).then((link) => {
+      return link.getText().then((linkText) => {
         expect(linkText.toLocaleLowerCase()).to.have.string('selenium');
       });
     });
@@ -45,30 +41,26 @@ defineSupportCode(function ({ Given, When, Then }) {
 defineSupportCode(function ({ Given, When, Then }) {
 
   Given('I am on the google home page - two', function () {
-    const world = this;
-    return world.driver.get('http://www.google.com').then(function () {
-      return world.driver.wait(until.elementLocated(By.css('body')), SELENIUM_TIMEOUT);
+    return this.driver.get('http://www.google.com').then(() => {
+      return this.driver.wait(until.elementLocated(By.css('body')), SELENIUM_TIMEOUT);
     });
   });
 
   When('I enter cucumber string into the search element', function () {
-    const world = this;
-    return world.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then(function (searchElement) {
+    return this.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then((searchElement) => {
       return searchElement.sendKeys('cucumberjs');
     });
   });
 
   When('I hit the RETURN key', function () {
-    const world = this;
-    return world.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then(function (searchElement) {
+    return this.driver.wait(until.elementLocated(By.id('lst-ib')), SELENIUM_TIMEOUT).then((searchElement) => {
       return searchElement.sendKeys(Key.ENTER);
     });
   });
 
   Then('I should see results including the strings cucumber and javascript', function () {
-    const world = this;
-    return world.driver.wait(until.elementLocated(By.css('#rso > div > div > div:nth-child(1) > div > div > h3 > a')), SELENIUM_TIMEOUT).then(function (link) {
-      return link.getText().then(function (linkText) {
+    return this.driver.wait(until.elementLocated(By.css('#rso > div > div > div:nth-child(1) > div > div > h3 > a')), SELENIUM_TIMEOUT).then((link) => {
+      return link.getText().then((linkText) => {
         expect(linkText.toLocaleLowerCase()).to.have.string('cucumber');
         expect(linkText.toLocaleLowerCase()).to.have.string('javascript');
       });
